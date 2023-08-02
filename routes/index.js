77a3a322857953611a23ef76a19cc4210bbcfb64
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-08-02 18:56:23 +0530

    Added jquery code in script.js to send request to server from frontend and display the data from frontend, improved looks of html

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ const upload = multer({
 const router=express.Router()
 
 router.get('/', (req, res)=>{
-    return res.render('home', {files: [], selectedFiles: []})
+    return res.render('home', {files: [], fileName: ''})
 })
 
 router.post('/files', upload.array('files'), function(req, res){
@@ -28,33 +28,43 @@ router.post('/files', upload.array('files'), function(req, res){
     let files=[]
     if(req.files.length>=0){
         req.files.forEach(file => {
-            files.push({name: file.originalname})
+            files.push({originalname: file.originalname, path: file.path})
         });
-        return res.render('home', { files: files, selectedFiles: []});
+        return res.render('home', { files: files, fileName: ''});
     }
     else{
-        return res.render('home', { files: [], selectedFiles: []});
+        return res.render('home', { files: [], fileName: ''});
     }
 
 })
 
 router.post('/selectedFiles', (req, res)=>{
     console.log('SELECTED FILES:', req.body)
-    let selectedFiles=[]
-    for (let key in req.body) {
-        console.log(key, req.body[key]);
-        selectedFiles.push(req.body[key])
-      }
+    const {fileName}=req.body
 
-    console.log('PATH OF SELECTED FILE:', path.join(__dirname+'/../'+'/uploads'+'/'+selectedFiles[0]))
+    let [filePath, originalname]=fileName.split(':')
 
-    let filePath=path.join(__dirname+'/../'+'/uploads'+'/'+selectedFiles[0])
+    console.log('PATH OF SELECTED FILE:', path.join(__dirname+'/../'+filePath), originalname)
+
+    filePath=path.join(__dirname+'/../'+filePath)
 
     fs.createReadStream(filePath)
     .on('data', function(data){
-        console.log(data)
+        //console.log('Data:', data.toString('utf-8'))
+        data=data.toString('utf-8').split('\n')
+        console.log('Data:', data, data.length)
+        let finalData=[]
+        data.forEach(row=>{
+            finalData.push(row.split(','))
+        })
+        console.log(finalData[0], finalData[0].length, finalData[0][1], finalData.length)
+        return res.status(200).json({
+            fileName: originalname,
+            data: finalData
+        })
+        //return res.render('displayTable', {data: finalData})
     })
 
-    return res.render('home', {files: [], selectedFiles})
+    
 })
 module.exports=router
