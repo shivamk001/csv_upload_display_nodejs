d8faec9aef6e31d5bf99d298d5b1aa3e8f5f0b4d
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-08-11 18:01:43 +0530

    Implemented pagination functionality

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,17 +1,34 @@
 let order=true
+let columnHeader=[]
 let finalData=[]
+let rowPerPage=5
+let fileName=''
+
 function toggleOrder(){
     order=!order
 }
 
+function setRowPerPage(rPP){
+    rowPerPage=rPP
+    console.log(rowPerPage)
+}
+
 function setData(data){
-    finalData=data
+    finalData=[...data]
+}
+
+function setColumnHeader(header){
+    columnHeader=[...header]
+}
+
+function setFileName(fN){
+    fileName=fN
 }
 
 function sortData(data, index){
     console.log("Order in sortData:", order)
-    let column=data[0]
-    data=data.slice(1)
+    console.log("Data in SortData:", data)
+
     data.sort(
          (a, b)=>{
             if(a[index]<b[index]){
@@ -37,31 +54,32 @@ function sortData(data, index){
     toggleOrder()
     console.log('Data:', data, order)
 
-    displayTable([column, ...data])
+    displayTable([...data])
 }
 
 function reset(data){
     displayTable(data)
 }
 
-
-function displayTable(data){
-    $('#columnRow').empty()
-    $('#tableBody').empty()
-    $('#searchByColumnValueForm').empty()
-    $('#resetButton').empty()
-    $('#header').empty()
-
-    console.log("Order in displayTable:", order)
-
+function createSelect(){
     let $select=$(`
-        <select name="column" id="columnSelect" required>
-            <option value="">Choose a Column</option>
-        </select>`
+    <select name="column" id="columnSelect" required>
+        <option value="">Choose a Column</option>
+    </select>`
     )
-    // style="display: none; 
-    data[0].forEach((column, indx) => {
 
+    columnHeader.forEach((column, indx) => {
+        let $op=$(`<option value="${indx}">${column}</option>`)
+        $select.append($op)
+    });
+    $('#searchByColumnValueForm').append('<p>Select rows per column:</p>')
+    $('#searchByColumnValueForm').append($select)
+    $('#searchByColumnValueForm').append('<input type="text" name="columnValue" id="columnValue" placeholder="Enter column value" required></input>')
+    $('#searchByColumnValueForm').append('<input type="submit">')
+}
+
+function createTable(data){
+    columnHeader.forEach((column, indx) => {
         let $th;
         if(order){
             $th=$(`<th value=${column}>${column} &nbsp <i class="fa-solid fa-angle-down"></i></th>`)
@@ -75,21 +93,21 @@ function displayTable(data){
             sortData(data, e.data.index)
         })
 
-        let $op=$(`<option value="${indx}">${column}</option>`)
+        
 
         $('#columnRow').append($th)
-        $select.append($op)
     });
 
-    data.slice(1).forEach(row=>{
+    data.forEach(row=>{
         let $tr= $('<tr>')
         row.forEach(col=>{
             $tr.append(`<td>${col}</td>`)
         })
         $('#tableBody').append($tr)
     })
+}
 
-    
+function createHeader(){
     $('#header').append(
         `
         <div style="display: flex; flex-direction: row; align-items: center;">
@@ -100,14 +118,88 @@ function displayTable(data){
                 <a href="/">Display another file</a>
             </button>
         </div>`)
+}
 
-    $('#searchByColumnValueForm').append($select)
-    $('#searchByColumnValueForm').append('<input type="text" name="columnValue" id="columnValue" placeholder="Enter column value" required></input>')
-    $('#searchByColumnValueForm').append('<input type="submit">')
+function pageButtonClick(event){
+    console.log('Page Button Clicked:', event.target.value)
+    let pageNumber=event.target.value
+    let upper=pageNumber*rowPerPage
+    let lower=upper-rowPerPage
+    console.log(lower, upper)
+    displayTable([...finalData.slice(lower, upper)])
+}
+
+function createSelectedFilesDiv(){
+    $('#displaySelectedFilesDiv').append(`<p>Selected FileName: ${fileName}</p>`)
+    $('#displaySelectedFilesDiv').append(`<p>Displaying ${rowPerPage} rows</p>`)
+}
+
+function createPaginationButtons(){
+    let len=finalData.length
+    let $paginationButtons=$('<div id="paginationButtonsDiv"></div>')
+    for(let i=0;i<len/rowPerPage;i++){
+        let $paginationButton=$(`<button class="paginationButton" value="${i+1}">${i+1}</button>`)
+        $paginationButton.on('click',  pageButtonClick)
+        $paginationButtons.append($paginationButton)
+    }
+    $('#paginationDiv').append($paginationButtons)
+}
+
+function changePagination(event){
+    let val=event.target.value
+    console.log('Row per page changed', val)
+    setRowPerPage(val)
+    displayTable(finalData.slice(0, val))
+}
+
+function createPaginationSelect(){
+
+    let $select=$(`    
+    <select value="paginationVal" id="pagination">
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="25">25</option>
+        <option value="50">50</option>
+        <option value="75">75</option>
+        <option value="100">100</option>
+    </select>`)
+    $select.on('change', changePagination)
+    $('#paginationDiv').append('<p>Select rows per column:</p>')
+    $('#paginationDiv').append($select)
     
+}
+
+function createResetButton(){
     let $button=$('<button type="button">Reset</button>')
     $button.on('click', function(){reset(finalData)})
     $('#resetButton').append($button)
+}
+
+function displayTable(data){
+    $('#tableDiv').css('display', 'block')
+    $('#columnRow').empty()
+    $('#tableBody').empty()
+    $('#searchByColumnValueForm').empty()
+    $('#resetButton').empty()
+    $('#header').empty()
+    $('#paginationDiv').empty()
+    $('#displaySelectedFilesDiv').empty()
+
+    console.log("Order in displayTable:", order)    
+    console.log("Data in displayTable:", data)
+    createSelect()
+
+    createTable(data)
+
+    createHeader()
+    
+    createResetButton()
+
+    createPaginationSelect()
+
+    createSelectedFilesDiv()
+
+    createPaginationButtons()
 }
 
 export function searchByColumnValueForm(event){
@@ -126,7 +218,7 @@ export function searchByColumnValueForm(event){
         return data[columnIndx]==columnValue
     })
     console.log(filteredData)
-    displayTable([column, ...filteredData])
+    displayTable([...filteredData])
     event.preventDefault();
 }
 
@@ -146,12 +238,14 @@ export function selectedFilesFormOnSubmit(event){
             success: function(dataa){
                 let {fileName, data}=dataa
                 console.log(fileName, data)
-                setData(data)
-                $('#displaySelectedFilesDiv').append(`<p>Selected FileName: ${fileName}</p>`)
+                setColumnHeader([...data[0]])
+                setData([...data.slice(1)])
+                setFileName(fileName)
+                
                 $('#selectedFilesDiv').remove();
                 $('#fileSubmitDiv').remove();
 
-                displayTable(data)
+                displayTable(finalData.slice(0, 5))
             }
         })
         event.preventDefault();
